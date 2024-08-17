eac30e2c238c82acb9d6b5a6e2ac97249b36a7b2
Endertitan100 <broenhill.oliver@gmail.com>
2024-08-18 00:03:00 +0100

    Added Flow Indicator

---
+++ enemy_spawner.gd
@@ -0,0 +1,14 @@
+extends MeshInstance2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Write code to clone enemy and damage player with a growinh hitbox 
+# Make some code that displays health and ability to collect and pickup scales
